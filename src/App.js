0d576faeab4f66cada3450cbcae5243cda061ca4
Ved Patel <138928150+VedPatel2811@users.noreply.github.com>
2024-06-19 07:19:39 +0000

    Footer is updated so that it can show the amount of packing is done in percentage.

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ export default function App() {
   function handleToggleItem(id) {
     setItems((items) =>
       items.map((item) =>
-        item.id === id ? { ...item, packed: !item.packed } : item
-      )
+        item.id === id ? { ...item, packed: !item.packed } : item,
+      ),
     );
   }
   return (
@@ -28,7 +28,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItems={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -46,7 +46,6 @@ function Form({ onAddItems }) {
 
     if (!description) return;
     const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log(newItem);
 
     onAddItems(newItem);
 
@@ -110,10 +109,24 @@ function Item({ item, onDeleteItem, onToggleItems }) {
     </li>
   );
 }
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats"> Start adding some items to your packing lsit 🚀</p>
+    );
+  const numItems = items.length;
+  const itemPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((itemPacked / numItems) * 100);
   return (
     <footer className="stats">
-      💼 You have X items on your list, and you already packed X (X%)
+      {percentage === 100 ? (
+        <em>You got everything! Ready to go ✈️</em>
+      ) : (
+        <em>
+          💼 You have {numItems} items on your list, and you already packed{" "}
+          {itemPacked} ({percentage}%)
+        </em>
+      )}
     </footer>
   );
 }
